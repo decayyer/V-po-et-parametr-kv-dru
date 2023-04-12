ed2837301e0870ab169b5bd0012b54ad01e9df88
decayyer <126061707+decayyer@users.noreply.github.com>
2023-04-12 09:16:09 +0200

    Add files via upload

---
+++ Parametry kvádru/Parametry kvádru/Kvádr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parametry_kvádru
+{
+    public class Kvádr
+    {
+        public double stranaA { get; set; }
+        public double stranaB { get; set; }
+        public double stranaC { get; set; }
+
+        public Kvádr(double a, double b, double c)
+        {
+            stranaA = a;
+            stranaB = b;
+            stranaC = c;
+        }
+
+        public double objem()
+        {
+            double vysledekObjem = (stranaA * stranaB * stranaC);
+            return vysledekObjem;
+        }
+        public double povrch()
+        {
+            double vysledekPovrch = (2 * (stranaA * stranaB + stranaA * stranaC + stranaB * stranaC));
+            return vysledekPovrch;
+        }
+        public double telesovaUhlopricka()
+        {
+            double vysledekTelesovaUhlopricka = (Math.Sqrt(stranaA * stranaA + stranaB * stranaB + stranaC * stranaC));
+            return vysledekTelesovaUhlopricka;
+        }
+        public double stenovaUhlopricka()
+        {
+            double vysledeStenovaUhlopricka = (Math.Sqrt(stranaA * stranaA + stranaB * stranaB));
+            return vysledeStenovaUhlopricka;
+        }
+    }
+  
+}
---
+++ Parametry kvádru/Parametry kvádru/Program.cs
@@ -0,0 +1,14 @@
+﻿using Parametry_kvádru;
+
+double stranaA = int.Parse(Console.ReadLine());
+double stranaB = int.Parse(Console.ReadLine());
+double stranaC = int.Parse(Console.ReadLine());
+Kvádr vysledky = new Kvádr(stranaA, stranaB, stranaC);
+
+Console.WriteLine(vysledky.objem());
+
+Console.WriteLine(vysledky.povrch());
+
+Console.WriteLine(vysledky.telesovaUhlopricka());
+
+Console.WriteLine(vysledky.stenovaUhlopricka());
